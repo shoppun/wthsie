ce179903d72f0b6c600c372aae7a3f0c9178139e
shoppun <112206495+shoppun@users.noreply.github.com>
2026-03-05 22:22:45 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'wthsie-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install: cache alle Dateien
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate: alten Cache löschen
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: zuerst Cache, dann Netzwerk
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
